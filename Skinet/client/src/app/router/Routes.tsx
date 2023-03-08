ad60bf7f33c4706db316c5012a6c2afaca9ebb75
VuAnh <anhnv4@smartosc.com>
2023-03-08 17:03:59 +0700

    add shopping cart feature v2 + add check out page

--- Skinet/client/src/app/router/Routes.tsx
+++ Skinet/client/src/app/router/Routes.tsx
@@ -3,6 +3,7 @@ import AboutPage from "../../features/about/AboutPage";
 import BasketPage from "../../features/basket/BasketPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import NotFound from "../errors/NotFound";
@@ -22,6 +23,7 @@ export const router = createBrowserRouter([
             {path: 'server-error', element: <ServerError/>},
             {path: 'not-found', element: <NotFound/>},
             {path: 'basket', element: <BasketPage/>},
+            {path: 'checkout', element: <CheckoutPage/>},
             {path: '*', element: <Navigate replace to='/not-found'/>}
         ]
     }
--- Skinet/client/src/features/catalog/ProductCard.tsx
+++ Skinet/client/src/features/catalog/ProductCard.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/StoreContext";
 import { Product } from "../../app/models/product";
+import { currencyFormat } from "../../app/util/util";
 
 interface Props {
     product: Product;
@@ -43,7 +44,7 @@ export default function ProductCard({ product }: Props) {
             />
             <CardContent>
                 <Typography gutterBottom color='secondary' variant="h5">
-                    ${(product.price / 100).toFixed(2)}
+                    {currencyFormat(product.price)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {product.productBrand} / {product.productType}
