733fea390e348e9c8a1b2e51cf5efc6c83020490
VuAnh <vuanhng2660@gmail.com>
2023-08-23 13:35:24 +0700

    remove react client

--- Skinet/client/src/features/home/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-export default function HomePage() {
-    return (
-        <Typography variant="h2">
-            Home page
-        </Typography>
-    )
-}
